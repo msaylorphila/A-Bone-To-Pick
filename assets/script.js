389fbd1e7bde9dda3bb0c668c27ff7f7c46179d5
ragingpenguin <eflinjh@gmail.com>
2023-01-11 12:33:18 -0500

    dogs now store to a list of objects in local storage for easier access later

--- assets/script.js
+++ assets/script.js
@@ -5,84 +5,102 @@ var inputEl = document.getElementById('zipInput');
 var dogFormEl = document.getElementById('dogForm')
 
 
-function getPetsByZip(event){
+function getPetsByZip(event) {
   event.preventDefault();
- 
+
   fetch("https://api.petfinder.com/v2/oauth2/token", {
-  body: "grant_type=client_credentials&client_id=" + pfApiKey + "&client_secret=" + pfSecret,
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
-  },
-  method: "POST"
-}).then(function (response) {
-    return response.json();
-})
-.then(function (credentials) {
-    console.log(credentials) 
-    var zipcode = inputEl.value;
-    var bla = "https://api.petfinder.com/v2/animals?type=dog&location=" + zipcode + "&sort=distance"
-    fetch(bla
-    , {
+    body: "grant_type=client_credentials&client_id=" + pfApiKey + "&client_secret=" + pfSecret,
     headers: {
-      Authorization: "Bearer " + credentials.access_token
-    }   
-  }).then(function (response){
-    return response.json()
-  }).then(function (data){
-    console.log(data)
-    getDogInfo(data)
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    method: "POST"
+  }).then(function (response) {
+    return response.json();
   })
-})
-//api.petfinder.com/v2/{CATEGORY}/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}
+    .then(function (credentials) {
+      console.log(credentials)
+      var zipcode = inputEl.value;
+      var bla = "https://api.petfinder.com/v2/animals?type=dog&location=" + zipcode + "&sort=distance"
+      fetch(bla
+        , {
+          headers: {
+            Authorization: "Bearer " + credentials.access_token
+          }
+        }).then(function (response) {
+          return response.json()
+        }).then(function (data) {
+          console.log(data)
+          getDogInfo(data)
+        })
+    })
+  //api.petfinder.com/v2/{CATEGORY}/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}
 }
 
 function getDogInfo(data) {
-    for (var i = 0; i <3; i++) {
-        var dogSelect = data.animals[i];
-        var name = dogSelect.name;
-        var age = dogSelect.age;
-        var contact = dogSelect.contact.email;
-        var descriptionFromPF = dogSelect.description;
-        var genderFromPF = dogSelect.gender;
-        var photo = dogSelect.primary_photo_cropped.full;// we'll have to convert this to a png
-        var status = dogSelect.status; //displays as "adoptable"
-        var breedsMixed = dogSelect.breeds.mixed; //displays as true/false
-        var breedsPrimary = dogSelect.breeds.primary;
-        var dogCardArr = [name, age, contact, descriptionFromPF, genderFromPF, photo, status, breedsMixed, breedsPrimary]
-        console.log(dogCardArr)
-        dogApiByBreed(breedsPrimary, genderFromPF) //added genderFromPF to pass it to dogApi for height/weight
-       
+  for (var i = 0; i < 3; i++) {
+    var dogSelect = data.animals[i];
+    var name = dogSelect.name;
+    var age = dogSelect.age;
+    var contact = dogSelect.contact.email;
+    var descriptionFromPF = dogSelect.description;
+    var genderFromPF = dogSelect.gender;
+    var photo = dogSelect.primary_photo_cropped.full;// we'll have to convert this to a png
+    var status = dogSelect.status; //displays as "adoptable"
+    var breedsMixed = dogSelect.breeds.mixed; //displays as true/false
+    var breedsPrimary = dogSelect.breeds.primary;
+    var currentDog = {
+      name: name,
+      age: age,
+      sex: genderFromPF,
+      photo: photo,
+      breed: breedsPrimary
+    }
+    console.log(currentDog)
+    var dogCardArr = [name, age, contact, descriptionFromPF, genderFromPF, photo, status, breedsMixed, breedsPrimary]
+    currentDogCard(currentDog)// console.log(dogCardArr)
+    // dogApiByBreed(breedsPrimary, genderFromPF) //added genderFromPF to pass it to dogApi for height/weight
+
+  }
 }
+function currentDogCard(currentDog) {
+  console.log(currentDog)
+  var dogCollection = JSON.parse(localStorage.getItem("dogCollectionArr"))
+  
+  if (dogCollection == null) {
+    dogCollection = [];
+  }; 
+  dogCollection.push(currentDog)
+  console.log(dogCollection)
+  localStorage.setItem('dogCollectionArr', JSON.stringify(dogCollection))
 }
 
 
 
 
 
-
-dogFormEl.addEventListener('submit', function (event) { getPetsByZip(event)})
+dogFormEl.addEventListener('submit', function (event) { getPetsByZip(event) })
 
 
 
 // *****************simple fetch for dog api, better to have variables for 
 // apiKey in the headers and greyhound(breed name) in url*****************
 
-function dogApiByBreed(breedsPrimary, genderFromPF){
-fetch('https://api.api-ninjas.com/v1/dogs?name=' + breedsPrimary, {
-  method: "GET",
-  headers: {"X-Api-Key" : "7VT9G3psGTVpzFOhgUZsag==6qGoaeaUyBn1jA8n"},
-  contentType: "application/json"
-})
-  .then(function (response) {
-    return response.json();
+function dogApiByBreed(breedsPrimary, genderFromPF) {
+  fetch('https://api.api-ninjas.com/v1/dogs?name=' + breedsPrimary, {
+    method: "GET",
+    headers: { "X-Api-Key": "7VT9G3psGTVpzFOhgUZsag==6qGoaeaUyBn1jA8n" },
+    contentType: "application/json"
   })
-  .then(function(data) {
-    console.log(data);
-    getDogStats(data, genderFromPF);
-  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      getDogStats(data, genderFromPF);
+    })
 }
 
-function getDogStats (data, genderFromPF) {
+function getDogStats(data, genderFromPF) {
   var breed = data[0];
   var barking = breed.barking;
   var energy = breed.energy;
