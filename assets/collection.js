aa372914c9a77c73694e03dd3957598a635e57bb
ragingpenguin <eflinjh@gmail.com>
2023-01-12 15:12:40 -0500

    added explicit function calls outside of for loop for easier explanation of issue

--- assets/collection.js
+++ assets/collection.js
@@ -1,30 +1,34 @@
 // GET https://api.petfinder.com/v2/animals/{id}
-var doggyDash = document.querySelector('.doggy-dash');
-var dogCollection = JSON.parse(localStorage.getItem("dogCollectionArr"));
+let doggyDash = document.querySelector('.doggy-dash');
+let dogCollection = JSON.parse(localStorage.getItem("dogCollectionArr"));
 for (i = 0; i < dogCollection.length; i++) {
-    let currentDog = dogCollection[i];
-    console.log(currentDog)
-    makeDogCard(currentDog);
+     let currentDog = dogCollection[i];
+     console.log(currentDog)
+     makeDogCard(currentDog);
 }
+    makeDogCard(dogCollection[1])
+    makeDogCard(dogCollection[2])
+    
+
 
 function makeDogCard(currentDog) {
-    var dogCardBorder = document.createElement('div');
-    var cardHeader = document.createElement('div');
-    var name = document.createElement('span');
-    var age = document.createElement('span');
-    var breed = document.createElement('span')
-    var photo = document.createElement('img');
-    var dogAttr = document.createElement('div');
-    var size = document.createElement('span');
-    var houseTrained = document.createElement('span');
-    var dogStats = document.createElement('div')
-    var dogCard = document.createElement('div');
-    var description = document.createElement('div');
-    var protectiveness = document.createElement('span');
-    var playfulness = document.createElement('span');
-    var trainability = document.createElement('span');
-    var energy = document.createElement('span')
-    var powerLevel = document.createElement('div')
+    let dogCardBorder = document.createElement('div');
+    let cardHeader = document.createElement('div');
+    let name = document.createElement('span');
+    let age = document.createElement('span');
+    let breed = document.createElement('span')
+    let photo = document.createElement('img');
+    let dogAttr = document.createElement('div');
+    let size = document.createElement('span');
+    let houseTrained = document.createElement('span');
+    let dogStats = document.createElement('div')
+    let dogCard = document.createElement('div');
+    let description = document.createElement('div');
+    let protectiveness = document.createElement('span');
+    let playfulness = document.createElement('span');
+    let trainability = document.createElement('span');
+    let energy = document.createElement('span')
+    let powerLevel = document.createElement('div')
     powerLevel.setAttribute('class', 'power-level')
     dogCard.setAttribute('class', "card");
     dogCardBorder.setAttribute('class', 'card-border');
@@ -58,64 +62,64 @@ function makeDogCard(currentDog) {
     }
     dogCard.appendChild(dogCardBorder);
     dogCardBorder.appendChild(cardHeader);
-    cardHeader.appendChild(name)
+    cardHeader.appendChild(name);
     cardHeader.appendChild(age);
     cardHeader.appendChild(breed);
-    dogCardBorder.appendChild(photo)
-    dogCardBorder.appendChild(dogAttr)
-    dogAttr.appendChild(size)
-    dogAttr.appendChild(houseTrained)
-    dogCardBorder.appendChild(dogStats)
-    dogStats.appendChild(description)
-    description.appendChild(energy)
-    description.appendChild(trainability)
-    description.appendChild(playfulness)
-    description.appendChild(protectiveness)
-    dogStats.appendChild(powerLevel)
-    // energyIcon(currentDog.energy)
-    // trainIcon(currentDog.trainability)
-    // playIcon(currentDog.playfulness)
-    // protectIcon(currentDog.protectiveness)
+    dogCardBorder.appendChild(photo);
+    dogCardBorder.appendChild(dogAttr);
+    dogAttr.appendChild(size);
+    dogAttr.appendChild(houseTrained);
+    dogCardBorder.appendChild(dogStats);
+    dogStats.appendChild(description);
+    description.appendChild(energy);
+    description.appendChild(trainability);
+    description.appendChild(playfulness);
+    description.appendChild(protectiveness);
+    dogStats.appendChild(powerLevel);
+    energyIcon(currentDog.energy);
+    trainIcon(currentDog.trainability);
+    playIcon(currentDog.playfulness);
+    protectIcon(currentDog.protectiveness);
 
     function energyIcon(num) {
-        var createSpan = document.createElement('span');
+        let createSpan = document.createElement('span');
         createSpan.setAttribute('class', 'item2');
         for (i = 0; i < num; i++) {
-            var createIcon = document.createElement('i');
+            let createIcon = document.createElement('i');
             createIcon.setAttribute('class', 'fa-solid fa-bolt-lightning');
             powerLevel.appendChild(createSpan);
             createSpan.appendChild(createIcon);
         }
     }
     function trainIcon(num) {
-        var createSpan = document.createElement('span');
+        let createSpan = document.createElement('span');
         createSpan.setAttribute('class', 'item2');
         for (i = 0; i < num; i++) {
-            var createIcon = document.createElement('i');
+            let createIcon = document.createElement('i');
             createIcon.setAttribute('class', ' fa-solid fa-poop');
             powerLevel.appendChild(createSpan);
             createSpan.appendChild(createIcon);
         }
     }
     function playIcon(num) {
-        var createSpan = document.createElement('span');
+        let createSpan = document.createElement('span');
         createSpan.setAttribute('class', 'item2');
         for (i = 0; i < num; i++) {
-            var createIcon = document.createElement('i');
+            let createIcon = document.createElement('i');
             createIcon.setAttribute('class', ' fa-solid fa-face-grin-tears');
             powerLevel.appendChild(createSpan);
             createSpan.appendChild(createIcon);
         }
     }
     function protectIcon(num) {
-        var createSpan = document.createElement('span');
+        let createSpan = document.createElement('span');
         createSpan.setAttribute('class', 'item2');
         for (i = 0; i < num; i++) {
-            var createIcon = document.createElement('i')
+            let createIcon = document.createElement('i')
             createIcon.setAttribute('class', 'fa-solid fa-shield');
             powerLevel.appendChild(createSpan);
             createSpan.appendChild(createIcon);
         }
     }
-    doggyDash.appendChild(dogCard)
+    doggyDash.appendChild(dogCard);
 }
